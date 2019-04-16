7a1af0989cc0fec3d0974e34e89c239fadffc4e4
Carlos Calderon <calderoncarloos@gmail.com>
2019-04-16 19:00:58 -0400

    Final Test v1.3

--- db.php
+++ db.php
@@ -3,3 +3,14 @@
 $conn = new PDO('mysql:host=127.0.0.1:49719; dbname=localdb', 'azure','6#vWHD_$');
 
 ?>
+
+<?php
+try{
+    $newdb = new PDO('mysql:host=127.0.0.1:49719; dbname=localdb', 'azure','6#vWHD_$');
+    echo "<p> Connection Succesfully! </p>";
+
+}
+catch(PDOException $e){
+    echo "<p> Connection Failed </p>";
+}
+?>
